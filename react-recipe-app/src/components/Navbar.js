ec68854cfcbf66aa2ca183c99d4bcd5502b60613
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-19 13:26:50 +0200

    navigation links

---
+++ react-recipe-app/src/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar(){
+    return(
+        <div className="navbar">
+            <a href="#!">Africa<span>Food</span>Cousine</a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">Settings</a>
+            </div>
+        </div>
+        
+    )
+}
