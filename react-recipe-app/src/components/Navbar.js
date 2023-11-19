db9790a7e3bddda12c37017932fe9ca89edfae2c
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-19 15:36:28 +0200

    sidebar icon

--- react-recipe-app/src/components/Navbar.js
+++ react-recipe-app/src/components/Navbar.js
@@ -1,4 +1,9 @@
+//import use effect
+import { useState } from "react"
+
 export default function Navbar(){
+    //state
+    const [showSidebar,, setShowSidebar] = useState(false)
     return(
         <div className="navbar container">
             <a href="#!" className="logo">African<span>Food</span>Cuisine</a>
@@ -8,8 +13,12 @@ export default function Navbar(){
                 <a href="#!">Settings</a>
             </div>
             {/* //sidebar */}
-            <div className="sidebar-btn">
-                hello
+            {/* data binding, if sidebar=true, have sidelist of sidebar & active, else have classlist of just show sidebar */}
+            {/* onclick event listener, when clicked, remove bars and show sidebar */}
+            <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
             </div>
         </div>
 
