b393df23b49bffe61b7119d4e1726a93de409d32
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-19 13:33:36 +0200

    logo

--- react-recipe-app/src/components/Navbar.js
+++ react-recipe-app/src/components/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar(){
     return(
         <div className="navbar container">
-            <a href="#!">Africa<span>Food</span>Cousine</a>
+            <a href="#!" className="logo">Africa<span>Food</span>Cousine</a>
             <div className="nav-links">
                 <a href="#!">Home</a>
                 <a href="#!">Recipes</a>
