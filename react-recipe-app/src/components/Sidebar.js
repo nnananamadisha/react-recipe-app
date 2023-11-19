7aabb253f3657fa94cb771cd81bc225a3c233926
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-19 16:43:49 +0200

    loop through links  array to get links + assign icons to links

--- react-recipe-app/src/components/Sidebar.js
+++ react-recipe-app/src/components/Sidebar.js
@@ -1,12 +1,12 @@
 //import font awesome icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-export default function Sidebar({links}){
+export default function Sidebar({links, close}){
     return(
-        <div className="sidebar">
+        <div className="sidebar" onClick={close}>
             {/* //loop through the links array */}
             { links.map(link => (
-                <a href="#!" key={link.name}>
+                <a className="sidebar-link" href="#!" key={link.name}>
                     <FontAwesomeIcon icon={link.icon} />
                     {link.name}
                 </a>
