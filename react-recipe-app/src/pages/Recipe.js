05f1f2744fa5826ea292b34ba40a9b16d6a56975
Jason <murangira99@gmail.com>
2023-11-20 23:39:47 +0200

    added pictures

--- react-recipe-app/src/pages/Recipe.js
+++ react-recipe-app/src/pages/Recipe.js
@@ -1,7 +1,91 @@
-export default function Recipe() {
-    return(
+import PreviousSearches from "../components/PreviousSearches"
+import RecipeCard from "../components/RecipeCard"
+
+export default function Recipes(){
+    const recipes = [
+        {
+            title: "Biltong",
+            image: "/imgages/biltong.jpg",
+        }, 
+        {
+            title: "Bobotie",
+            image: "/img/bobotie.jpg",
+        },
+        {
+            title: "Bunnychow",
+            image: "/img/bunnychow.jpg",
+        },
+        {
+            title: "Chicken-feet",
+            image: "/img/chicken-feet.jpg",
+        },
+        {
+            title: "Chakalaka",
+            image: "/img/chakalaka.jpg",
+        },
+        {
+            title: "Carrot-cake",
+            image: "/img/carrot-cake.jpg",
+        }, 
+        {
+            title: "Lamb-sosaties",
+            image: "/img/lamb-sosaties.jpg",
+        },
+        {
+            title: "Samp-and-beans",
+            image: "/img/samp-and-beans.jpg",
+        },
+        {
+            title: "Malva-pudding",
+            image: "/img/malva-pudding.jpg",
+        },
+        {
+            title: "Biltong",
+            image: "/imgages/biltong.jpg",
+        }, 
+        {
+            title: "Bobotie",
+            image: "/img/bobotie.jpg",
+        },
+        {
+            title: "Bunnychow",
+            image: "/img/bunnychow.jpg",
+        },
+        {
+            title: "Chicken-feet",
+            image: "/img/chicken-feet.jpg",
+        },
+        {
+            title: "Chakalaka",
+            image: "/img/chakalaka.jpg",
+        },
+        {
+            title: "Carrot-cake",
+            image: "/img/carrot-cake.jpg",
+        }, 
+        {
+            title: "Lamb-sosaties",
+            image: "/img/lamb-sosaties.jpg",
+        },
+        {
+            title: "Samp-and-beans",
+            image: "/img/samp-and-beans.jpg",
+        },
+        {
+            title: "Malva-pudding",
+            image: "/img/malva-pudding.jpg",
+        }
+    ].sort(() => Math.random() - 0.5)
+
+    return (
         <div>
-            Recipe page
+            <PreviousSearches />
+            <div className="recipes-container">
+                {/* <RecipeCard /> */}
+                {recipes.map((recipe, index) => (
+                    <RecipeCard key={index} recipe={recipe} />
+                ))}
+            </div>
         </div>
     )
 }
