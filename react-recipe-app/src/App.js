d3ca48e465ce713383da978bc08225e3bf736404
Jason <murangira99@gmail.com>
2023-11-20 15:56:25 +0200

    added 4 imports

--- react-recipe-app/src/App.js
+++ react-recipe-app/src/App.js
@@ -1,4 +1,16 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import Settings from "./pages/Settings";
+
 
 
 function App() {
