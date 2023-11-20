631532a6b938067a75f96d0ef4dd8d79c993b6c1
Nnanana Madisha <nnananamadisha99@gmail.com>
2023-11-20 11:54:08 +0200

    added code

---
+++ react-recipe-app/src/components/Footer.js
@@ -0,0 +1,23 @@
+export default function Footer(){
+    return (
+        <div className="footer container">
+            <div className="footer-section">
+                <p className="title">AfricanCuisineHub.com</p>
+                <p>AfricanCuisineHub is where you can satisfy both your soul and palate with a delightful array of recipes from diverse African cuisines. Plus, enjoy our services completely free of charge."</p>
+                <p>&copy; 2023 | All Rights Reserved</p>
+            </div>
+            <div className="footer-section">
+                <p className="title">Contact Us</p>
+                <p>AfricanCuisineHub@gmail.com</p>
+                <p>+27-5324-9454</p>
+                <p>52 Emporer Street,Johannesburg</p>
+            </div>
+            <div className="footer-section">
+                <p className="title">Socials</p>
+                <p>Facebook</p>
+                <p>Twitter</p>
+                <p>Instagram</p>
+            </div>
+        </div>
+    )
+}
