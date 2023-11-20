23e773f250d34f056ea854ec76829ee366500c93
Jason <murangira99@gmail.com>
2023-11-20 17:04:10 +0200

    added about for imports

--- react-recipe-app/src/components/Sidebar.js
+++ react-recipe-app/src/components/Sidebar.js
@@ -1,16 +1,16 @@
 //import font awesome icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
+import { Link, useLocation } from "react-router-dom"
 export default function Sidebar({links, close}){
-    return(
+    const location = useLocation()
+    return (
         <div className="sidebar" onClick={close}>
-            {/* //loop through the links array */}
             { links.map(link => (
-                <a className="sidebar-link" href="#!" key={link.name}>
+                <Link to={link.path} className={location.pathname === link.path ? "sidebar-link active" : "sidebar-link"} key={link.name}>
                     <FontAwesomeIcon icon={link.icon} />
                     {link.name}
-                </a>
+                </Link>
             )) }
         </div>
-    )
+    );
 }
