ed5d179dcb382d09370a30be9d9ea252a0be5b1b
Jason <murangira99@gmail.com>
2023-11-20 19:30:18 +0200

    imported customimage

--- react-recipe-app/src/components/RecipeCard.js
+++ react-recipe-app/src/components/RecipeCard.js
@@ -1,7 +1,14 @@
-export default function Recipe() {
-    return(
-        <div>
-            Recipe page
+import CustomImage from "./CustomImage"
+
+export default function RecipeCard({recipe}){
+    return (
+        <div className="recipe-card">
+            <CustomImage imgSrc={recipe.image} pt="65%"/>
+            <div className="recipe-card-info">
+                <p className="recipe-title">{recipe.title}</p>
+                <p className="recipe-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <a className="view-btn" href="#!">VIEW RECIPE</a>
+            </div>
         </div>
-    )
+    );
 }
