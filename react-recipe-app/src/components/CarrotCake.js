7235ceabd7632d24af21bd9cf6377027fa1b85ce
Nnanana Madisha <nnananamadisha99@gmail.com>
2023-11-21 11:13:25 +0200

    added a component

---
+++ react-recipe-app/src/components/CarrotCake.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const CarrotCake = () => {
+    return (
+        <div>
+            <h2>Carrot Cake</h2>
+            <p>Ingredients:</p>
+            <ul>
+                <li>2 cups grated carrots</li>
+                <li>2 cups all-purpose flour</li>
+                <li>1 ½ cups granulated sugar</li>
+                <li>1 cup vegetable oil</li>
+                <li>4 eggs</li>
+                <li>1 teaspoon baking powder</li>
+                <li>1 teaspoon baking soda</li>
+                <li>1 teaspoon ground cinnamon</li>
+                <li>½ teaspoon salt</li>
+                <li>1 cup chopped nuts (optional)</li>
+                <li>1 cup crushed pineapple, drained</li>
+                {/* Add more ingredients as needed */}
+            </ul>
+            <p>Instructions:</p>
+            <ol>
+                <li>Preheat oven to 350°F (175°C). Grease and flour a cake pan.</li>
+                <li>In a bowl, mix flour, baking powder, baking soda, cinnamon, and salt.</li>
+                <li>In another bowl, whisk together sugar, oil, and eggs. Add grated carrots, nuts, and pineapple.</li>
+                <li>Gradually add the dry ingredients to the wet ingredients and mix until well combined.</li>
+                <li>Pour the batter into the prepared pan and bake for 30-40 minutes or until a toothpick comes out clean.</li>
+                <li>Let the cake cool completely before frosting. Enjoy!</li>
+                {/* Add more instructions as needed */}
+            </ol>
+        </div>
+    );
+};
+
+export default CarrotCake;
---
+++ react-recipe-app/src/components/ChickenFeet.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const ChickenFeet = () => {
+    return (
+        <div>
+            <h2>South African Chicken Feet</h2>
+            <p>Ingredients:</p>
+            <ul>
+                <li>2 lbs chicken feet, cleaned</li>
+                <li>2 onions, finely chopped</li>
+                <li>4 cloves garlic, minced</li>
+                <li>1 tablespoon ginger, grated</li>
+                <li>1 teaspoon paprika</li>
+                <li>1 teaspoon curry powder</li>
+                <li>1 teaspoon cayenne pepper (adjust to taste)</li>
+                <li>Salt and pepper to taste</li>
+                <li>2 tablespoons vegetable oil</li>
+                {/* Add more ingredients as needed */}
+            </ul>
+            <p>Instructions:</p>
+            <ol>
+                <li>Heat oil in a pan and sauté onions, garlic, and ginger until fragrant.</li>
+                <li>Add chicken feet, paprika, curry powder, cayenne pepper, salt, and pepper. Cook until browned.</li>
+                <li>Add enough water to cover the chicken feet. Simmer until tender.</li>
+                <li>Adjust seasoning to taste. Serve hot.</li>
+                {/* Add more instructions as needed */}
+            </ol>
+        </div>
+    );
+};
+
+export default ChickenFeet;
