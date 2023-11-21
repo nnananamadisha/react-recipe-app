f83efafc16c1bd3515709ac2629bc6b2a217f442
Nnanana Madisha <nnananamadisha99@gmail.com>
2023-11-21 11:25:11 +0200

    Added changes

--- react-recipe-app/src/components/RecipeCard.js
+++ react-recipe-app/src/components/RecipeCard.js
@@ -1,12 +1,26 @@
-import CustomImage from "./CustomImage"
+import React from 'react';
+import CustomImage from './CustomImage';
+import ChickenFeet from './ChickenFeet';
+import Chakalaka from './Chakalaka';
+import CarrotCake from './CarrotCake';
 
-export default function RecipeCard({recipe}){
+export default function RecipeCard({ recipe }) {
+    const recipeComponents = {
+        ChickenFeet: <ChickenFeet />,
+        chakalaka: <Chakalaka />,
+        carrotCake: <CarrotCake />,
+        
+    };
+
+    const recipeComponent = recipeComponents[recipe.type] || null;
+
     return (
         <div className="recipe-card">
-            <CustomImage imgSrc={recipe.image} pt="65%"/>
+            <CustomImage ImgSrcmgSrc={recipe.image} pt="65%" />
             <div className="recipe-card-info">
                 <p className="recipe-title">{recipe.title}</p>
-                <p className="recipe-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <p className="recipe-desc">{recipe.description}</p>
+                {recipeComponent}
                 <a className="view-btn" href="#!">VIEW RECIPE</a>
             </div>
         </div>
