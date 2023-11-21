8ce9734ab4e4509a44d6d7fedac1ed9a232a719f
Nnanana Madisha <nnananamadisha99@gmail.com>
2023-11-21 07:07:51 +0200

    corrected typing error

--- react-recipe-app/src/App.js
+++ react-recipe-app/src/App.js
@@ -9,7 +9,7 @@ import Navbar from "./components/Navbar"
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
-import Recipes from "./pages/Recipes";
+import Recipes from "./pages/Recipe";
 import Settings from "./pages/Settings";
 
 
