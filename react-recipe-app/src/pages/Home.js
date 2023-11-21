59199655468d504635c62623acaed0eba9cfb5d1
Nnanana Madisha <nnananamadisha99@gmail.com>
2023-11-21 07:08:16 +0200

    corrected typing error

--- react-recipe-app/src/pages/Home.js
+++ react-recipe-app/src/pages/Home.js
@@ -1,16 +1,12 @@
 import HeroSection from "../components/HeroSection";
-import ImproveSkills from "../components/ImproveSkills";
-import QouteSection from "../components/QuoteSection";
-import ChiefsSection from "../components/ChiefsSection";
+
 
 
 export default function Home(){
     return (
         <div>
             <HeroSection />
-            <ImproveSkills />
-            <QouteSection />
-            <ChiefsSection />
+            
         </div>
     )
 }
