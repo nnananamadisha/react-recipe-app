2c88d710bc5b125cb5c9a99e27e0f8b3b94ff6a8
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-22 11:39:13 +0200

    changes

--- react-recipe-app/src/components/RecipeCard.js
+++ react-recipe-app/src/components/RecipeCard.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import CustomImage from './CustomImage';
-import ChickenFeet from './ChickenFeet';
-import Chakalaka from './Chakalaka';
-import CarrotCake from './CarrotCake';
 
 export default function RecipeCard({ recipe }) {
-    const recipeComponents = {
-        ChickenFeet: <ChickenFeet />,
-        chakalaka: <Chakalaka />,
-        carrotCake: <CarrotCake />,
-        
-    };
-
-    const recipeComponent = recipeComponents[recipe.type] || null;
-
     return (
         <div className="recipe-card">
             <CustomImage ImgSrc={recipe.image} pt="65%" />
@@ -23,8 +11,6 @@ export default function RecipeCard({ recipe }) {
                 <p className="recipe-desc">{recipe.description}</p>
             </div>
                 
-            {recipeComponent}
-            
             <a className="view-btn" href="#!">Ingredients</a>
             <p className='recipe-list'>{recipe.ingredients}</p>
             {/* </div> */}
