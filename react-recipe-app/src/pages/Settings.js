289c3cf2570402892cc1ff7dc774e5ec5ddb5b60
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-22 10:57:00 +0200

    change primary color

--- react-recipe-app/src/pages/Settings.js
+++ react-recipe-app/src/pages/Settings.js
@@ -7,7 +7,7 @@ export default function Settings(){
     const [settings, setSettings] = useState({
         "--background-color": "#fff",
         "--background-light": "#fff",
-        "--primary-color": "rgb(255, 0, 86)",
+        "--primary-color": "rgb(0, 139, 139)",
         "--shadow-color": "rgba(0,0,0,0.2)",
         "--text-color": "#0A0A0A",
         "--text-light": "#575757",
@@ -76,7 +76,7 @@ export default function Settings(){
 
 
     const primaryColors = [
-        "rgb(255, 0, 86)",
+        "rgb(0, 139, 139)",
         "rgb(33, 150, 243)",
         "rgb(255, 193, 7)",
         "rgb(0, 200, 83)",
